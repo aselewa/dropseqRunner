509d8b2982fbeb0d4e871454ea9b5d16195b4209
aselewa <iamselewa@gmail.com>
2019-11-18 13:40:30 -0600

    Update makeref.py

--- makeref.py
+++ makeref.py
@@ -1,5 +1,6 @@
 
 import os
+import shutil
 import argparse
 
 if __name__ == '__main__':
@@ -11,21 +12,22 @@ if __name__ == '__main__':
     parser.add_argument('--cluster', action='store_true', help='Add this flag if the job should run on the cluster.')
     args = parser.parse_args()
     
-    if args.fasta == None or args.gtf == None:
-      msg='Please provide a valid fasta file, and a genome annotation file in GTF format.'
-      raise Exception(msg)
-    
-    if os.path.exists(args.outDir):
-      os.system(f'rm -r {args.outDir}')
+    assert os.file.exists(args.fasta), 'Please provide a valid fasta file.' 
+    assert os.file.exists(args.gtf), 'Please provide a valid gtf file.' 
+    assert not os.dir.exists(args.outDir), \
+    'Cannot create output directory because it already exists. Please provide the location and name of a non-existing directory.')
+    assert shutil.which('gtfToGenePred') is not None, \
+    'gtfToGenePred not found. Did you forget to activate the conda environment? Use the conda environment in environment.yaml to quickly install all the required software.' 
     
     print('Setting up directory and creating auxililary files..')
+    
     os.system(f'mkdir {args.outDir}')
-    os.system(f'source activate dropRunner; gtfToGenePred {args.gtf} tmp -genePredExt')
+    os.system(f'gtfToGenePred {args.gtf} tmp -genePredExt')
     cmd = f"""awk '{{print $12"\t"$0}}' tmp | cut -f1-11 > {args.outDir}/refFlat_for_picard.refFlat; rm tmp"""
     os.system(cmd)
      
     if os.stat(f'{args.outDir}/refFlat_for_picard.refFlat').st_size == 0:
-        msg = 'FAILED to create auxililary files. Check above for error messages. Make sure miniconda3/bin/ is in your path, and make sure that you supplied a valid GTF file.'
+        msg = 'FAILED to create auxililary files. Check above for error messages. Make sure that you supplied a valid GTF file.'
         raise Exception(msg)
 
     if args.cluster:
@@ -53,4 +55,4 @@ STAR --runThreadN 4 --runMode genomeGenerate --genomeDir {args.outDir}/ --genome
     else:
       
       print('Genome index generation will run locally on this machine. This may not complete due to STARs large memory requirement.')
-      os.system(f'source activate dropRunner; STAR --runThreadN 1 --runMode genomeGenerate --genomeDir {args.outDir}/ --genomeFastaFiles {args.fasta} --sjdbGTFfile {args.gtf} --sjdbOverhang 59')
+      os.system(f'STAR --runThreadN 1 --runMode genomeGenerate --genomeDir {args.outDir}/ --genomeFastaFiles {args.fasta} --sjdbGTFfile {args.gtf} --sjdbOverhang 59')
